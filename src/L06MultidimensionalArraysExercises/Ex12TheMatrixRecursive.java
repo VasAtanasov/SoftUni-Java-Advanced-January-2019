1af88ca00facf447d3d038421c3e5179e2299054
Vasil Atanasov <vas.atanasov@gmail.com>
2019-01-20 09:25:41 +0200

    added tasks

--- src/L06MultidimensionalArraysExercises/Ex12TheMatrixRecursive.java
+++ src/L06MultidimensionalArraysExercises/Ex12TheMatrixRecursive.java
@@ -6,56 +6,48 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
-//90/100 Test 01 gives time limit
+
 @SuppressWarnings("Duplicates")
-public class Ex12TheMatrix {
+public class Ex12TheMatrixRecursive { // 100/100
     private static BufferedReader reader;
     private static String[][] maze;
     private static int[] rowOffset;
     private static int[] colOffset;
     private static Deque<Integer> possibleMoves;
- 
+    private static String startChar;
+    private static String fillChar;
+
     static {
         reader = new BufferedReader(new InputStreamReader(System.in));
-        colOffset = new int[] {- 1, 1, 0, 0};
         rowOffset = new int[] {0, 0, - 1, 1};
+        colOffset = new int[] {- 1, 1, 0, 0};
         possibleMoves = new ArrayDeque<>();
     }
 
     public static void main(String[] args) throws IOException {
         readMatrix();
-        String fillChar = reader.readLine();
+        fillChar = reader.readLine();
         int[] startParams = getParams();
         int starRow = startParams[0];
         int starCol = startParams[1];
 
-        if (! inRange(starRow, starCol)) {
+        startChar = maze[starRow][starCol];
+
+        traversCell(starRow, starCol);
+
+        printMatrix();
+    }
+
+    private static void traversCell(int row, int col) {
+        if (! inRange(row, col) || ! startChar.equals(maze[row][col])) {
             return;
         }
 
-        String startChar = maze[starRow][starCol];
-
-        possibleMoves.addLast(starRow);
-        possibleMoves.addLast(starCol);
-
-        while (! possibleMoves.isEmpty()) {
-            int currentRow = possibleMoves.removeFirst();
-            int currentCol = possibleMoves.removeFirst();
-
-            for (int i = 0; i < rowOffset.length; i++) {
-                int row = currentRow + rowOffset[i];
-                int col = currentCol + colOffset[i];
-
-                if (inRange(row, col) && maze[row][col].equals(startChar)) {
-                    possibleMoves.addLast(row);
-                    possibleMoves.addLast(col);
-                }
-            }
-
-            maze[currentRow][currentCol] = fillChar;
-        }
-
-        printMatrix();
+        maze[row][col] = fillChar;
+        traversCell(row + rowOffset[0], col + colOffset[0]);
+        traversCell(row + rowOffset[1], col + colOffset[1]);
+        traversCell(row + rowOffset[2], col + colOffset[2]);
+        traversCell(row + rowOffset[3], col + colOffset[3]);
     }
 
     private static void printMatrix() {
