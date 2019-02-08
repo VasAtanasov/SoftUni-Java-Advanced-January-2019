36a552e5b930a413f6c393cfbf3b399ac560f601
Vasil Atanasov <vas.atanasov@gmail.com>
2019-02-08 10:18:41 +0200

    added tasks and resources

--- src/L17Generics/Ex02GenericArrayCreator/ArrayCreator.java
+++ src/L17Generics/Ex02GenericArrayCreator/ArrayCreator.java
@@ -3,12 +3,14 @@ package L17Generics.Ex02GenericArrayCreator;
 import java.lang.reflect.Array;
 
 public class ArrayCreator {
-    public static <T> T[] create(int length, T item) { // wrong method not working but needed int judge
-        Object[] array = new Object[length];
+
+    @SuppressWarnings("unchecked")
+    public static <T> T[] create(int length, T item) {
+        T[] array = (T[]) new Object[length];
         for (int i = 0; i < length; i++) {
             array[i] = item;
         }
-        return (T[]) array;
+        return array;
     }
 
     @SuppressWarnings("unchecked")
